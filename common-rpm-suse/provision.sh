80dbf8b2a23cc609674e53e5439915368f795641
David Trudgian <dave@trudgian.net>
2018-04-06 20:40:02 -0500

    Add opensuse 42.3

---
+++ common-rpm-suse/provision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Now some base repo stuff
+zypper --non-interactive install automake autoconf libarchive-devel openssl-devel libuuid-devel git valgrind vim-enhance squashfsd
+
+zypper --non-interactive install go go-doc
+
